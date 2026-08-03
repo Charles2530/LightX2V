981e661529e732bc043b6254285c3b9ba2aeb1ea
Charles2530 <2569337619@qq.com>
2026-08-03 11:50:06 +0800

    initial fastwam

--- lightx2v_train/scripts/run_fastwam_libero_train.sh
+++ lightx2v_train/scripts/run_fastwam_libero_train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0,1,2,3}
+export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0,1,2,3,4,5,6,7}
 
-NPROC_PER_NODE=${NPROC_PER_NODE:-4}
+NPROC_PER_NODE=${NPROC_PER_NODE:-8}
 
 torchrun \
 --standalone \
 --nproc_per_node="${NPROC_PER_NODE}" \
-train.py --config configs/train/fastwam/libero_uncond_2cam224.yaml
+train.py --config configs/train/fastwam/libero_uncond_2cam224_test.yaml
